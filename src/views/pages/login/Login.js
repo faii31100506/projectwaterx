5512298ac148984189670bed1135519f32e02b95
Zexzeed93 <134593217+Zexzeed93@users.noreply.github.com>
2024-01-19 16:07:07 +0700

    Update

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import {
   CButton,
   CCard,
@@ -10,69 +10,78 @@ import {
   CForm,
   CFormInput,
   CInputGroup,
-  CInputGroupText,
   CRow,
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilLockLocked, cilUser } from '@coreui/icons'
+} from "@coreui/react";
+import Swal from "sweetalert2";
+// import "src/views/pages/login/Login.css";
+import "../../../views/pages/login/Login.css";
 
 const Login = () => {
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // alert("Pass");
+    if (Username == "LnwZa" && Password == "007x") {
+      Swal.fire("Login Success!", "Welcome to Water X Project", "success");
+      setUsername("");
+      setPassword("");
+    } else {
+      Swal.fire("Sorry!", "Can't login, please try again", "error");
+      setUsername("");
+      setPassword("");
+    }
+  };
+
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
+    <div className="background">
+      <CContainer className="CardGroup">
         <CRow className="justify-content-center">
           <CCol md={8}>
             <CCardGroup>
-              <CCard className="p-4">
+              <CCard className="Card">
                 <CCardBody>
                   <CForm>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
+                    <div className="Loghead">
+                      <h1
+                        className="Logintext"
+                        style={{ marginBottom: "0rem" }}
+                      >
+                        Log in
+                      </h1>
+                    </div>
+
+                    <p className="welcometext">ยินดีต้อนรับสู่ Water X</p>
+
                     <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
+                      <CFormInput
+                        className="textInput"
+                        placeholder="Username"
+                        autoComplete="username"
+                        value={Username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
                       <CFormInput
+                        style={{ marginBottom: "10px" }}
+                        className="textInput"
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
+                        value={Password}
+                        onChange={(e) => setPassword(e.target.value)}
                       />
                     </CInputGroup>
+
                     <CRow>
-                      <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
-                          Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
+                      <CCol>
+                        <CButton className="px-4" onClick={handleLogin}>
+                          LOGIN
                         </CButton>
                       </CCol>
                     </CRow>
                   </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
                 </CCardBody>
               </CCard>
             </CCardGroup>
@@ -80,7 +89,7 @@ const Login = () => {
         </CRow>
       </CContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
